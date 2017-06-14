ebb6fd7818c05de920f18d887a7bd35421e9a032
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-06-14 11:24:44 -0300

    Remoção do laravelcollective para geração de formulário programaticamente.

--- resources/views/forms/curso.blade.php
+++ resources/views/forms/curso.blade.php
@@ -1,21 +1,24 @@
 @if ($curso->id)
-    {!! Form::model($curso, ['route' => ['cursos.update', $curso->id], 'method' => 'PUT']) !!}
+    <form action="{{ route('cursos.update', $curso->id) }}" method="post">
+        {{ method_field('PUT') }}
 @else
-    {!! Form::model($curso, ['route' => 'cursos.store']) !!}
+    <form action="{{ route('cursos.store') }}" method="post">
+        {{ method_field('POST') }}
 @endif
+    {{ csrf_field() }}
     <div class="form-group{{ count($errors->get('nome')) > 0 ? ' has-error' : '' }}">
-        {!! Form::label('nome', 'Nome') !!}
-        {!! Form::text('nome', null, ['class' => 'form-control']) !!}
+        <label for="inputNome">Nome</label>
+        <input type="text" name="nome" value="{{ old('nome', $curso->nome) }}" id="inputNome" class="form-control">
         @foreach ($errors->get('nome') as $message)
             <span class="help-block">{{ $message }}</span>
         @endforeach
     </div>
     <div class="form-group{{ count($errors->get('apresentacao')) > 0 ? ' has-error' : '' }}">
-        {!! Form::label('apresentacao', 'Apresentação') !!}
-        {!! Form::textarea('apresentacao', null, ['class' => 'form-control']) !!}
+        <label for="textareaApresentacao">Apresenta&ccedil;&atilde;o</label>
+        <textarea name="apresentacao" rows="5" id="textareaApresentacao" class="form-control">{{ old('apresentacao', $curso->apresentacao) }}</textarea>
         @foreach ($errors->get('apresentacao') as $message)
             <span class="help-block">{{ $message }}</span>
         @endforeach
     </div>
-    {!! Form::submit($submit, ['class' => 'btn btn-primary']) !!}
-{!! Form::close() !!}
+    <button type="submit" class="btn btn-primary">{{ $submit }}</button>
+</form>
