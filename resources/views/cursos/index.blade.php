3b63aceca47fca67d588fc933e0bcf7622bd8e53
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-06-14 12:28:13 -0300

    Ajuste no alert, mudança do cabeçalho da coluna de ID, ajustes na exibição das ofertas e adição de classe no formulário de remoção para habilitar a confirmação.

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        <div class="col-xs-12">
-            @if (Session::has('status'))
-                <div class="alert alert-success">
+    @if (Session::has('status') && Session::has('message'))
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="alert alert-{{ Session::get('status') }}">
                     <button type="button" class="close" data-dismiss="alert" aria-label="Fechar">
                         <span aria-hidden="true">&times;</span>
                     </button>
-                    {{ Session::get('status') }}
+                    <p>{{ Session::get('message') }}</p>
                 </div>
-            @endif
+            </div>
         </div>
-    </div>
+    @endif
     <div class="row">
         <div class="col-xs-12 col-md-10">
             <h2>Cursos</h2>
@@ -24,7 +24,7 @@
     <table class="table table-hover table-cursos">
         <thead>
             <tr>
-                <th>#</th>
+                <th>ID</th>
                 <th>Nome</th>
                 <th>Ofertas</th>
                 <th>Criado em</th>
@@ -46,20 +46,26 @@
                             </div>
                             <div class="modal-body">
                                 <p>{{ $curso->apresentacao }}</p>
-                                <h4>Ofertas <span class="badge">{{ count($curso->ofertas) }}</span></h4>
-                                <ol>
-                                @foreach ($curso->ofertas as $oferta)
-                                    <li>
-                                        <strong>{{ $oferta->campus->nome }}:</strong> {{ $oferta->modalidade->nome }} - {{ $oferta->nivel->pai->nome }} > {{ $oferta->nivel->nome }} -
-                                    @foreach ($oferta->turnos as $turno)
-                                        {{ $turno->nome }}
-                                        @if (!$loop->last)
-                                            {{ ', ' }}
-                                        @endif
+                                @if (count($curso->ofertas) > 0)
+                                    <h4>Ofertas <span class="badge">{{ count($curso->ofertas) }}</span></h4>
+                                    <ol>
+                                    @foreach ($curso->ofertas as $oferta)
+                                        <li>
+                                            <strong>{{ $oferta->campus->nome }}:</strong> {{ $oferta->modalidade->nome }} - {{ $oferta->nivel->pai->nome }} > {{ $oferta->nivel->nome }} -
+                                        @foreach ($oferta->turnos as $turno)
+                                            {{ $turno->nome }}
+                                            @if (!$loop->last)
+                                                {{ ', ' }}
+                                            @endif
+                                        @endforeach
+                                        </li>
                                     @endforeach
-                                    </li>
-                                @endforeach
-                                </ol>
+                                    </ol>
+                                @else
+                                    <div class="alert alert-info">
+                                        <p>Esse Curso ainda n&atilde;o possui ofertas cadastradas.</p>
+                                    </div>
+                                @endif
                             </div>
                             <div class="modal-footer">
                                 <p><strong>&Uacute;ltima Modifica&ccedil;&atilde;o:</strong> {{ $curso->updated_at ? $curso->updated_at->format('d/m/Y \à\s h:i') : '-' }}</p>
@@ -71,14 +77,14 @@
                 <td>{{ $curso->created_at ? $curso->created_at->format('d/m/Y h:i'): '-' }}</td>
                 <td>{{ $curso->updated_at ? $curso->updated_at->format('d/m/Y h:i') : '-' }}</td>
                 <td class="text-center">
-                    <div class="btn-group">
-                        <a href="{{ route('cursos.edit', $curso->id) }}" class="btn btn-xs btn-default" title="Editar"><span class="glyphicon glyphicon-edit"></span><span class="sr-only">Editar</span></a>
-                        <form action="{{ route('cursos.destroy', $curso->id) }}" method="post">
-                            {{ csrf_field() }}
-                            {{ method_field('DELETE') }}
-                            <button type="submit" name="submit" value="delete" class="btn btn-xs btn-danger"><span class="glyphicon glyphicon-remove"></span><span class="sr-only">Deletar</span></button>
-                        </form>
-                    </div>
+                    <form action="{{ route('cursos.destroy', $curso->id) }}" method="post" class="delete">
+                        {{ method_field('DELETE') }}
+                        {{ csrf_field() }}
+                        <div class="btn-group">
+                            <a href="{{ route('cursos.edit', $curso->id) }}" class="btn btn-xs btn-default" title="Editar Curso"><span class="glyphicon glyphicon-edit"></span><span class="sr-only">Editar Curso</span></a>
+                            <button type="submit" class="btn btn-xs btn-danger" title="Deletar Curso"><span class="glyphicon glyphicon-remove"></span><span class="sr-only">Deletar Curso</span></button>
+                        </div>
+                    </form>
                 </td>
             </tr>
         @endforeach
