2d74e2a60f8aea185e4dec15d16abd8dbfab66d4
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-06-16 12:02:34 -0300

    Adição de favicons estáticos e pré-compilados.

---
+++ resources/views/partials/favicons.blade.php
@@ -0,0 +1,12 @@
+<link rel="apple-touch-icon" sizes="180x180" href="{{ URL::asset('favicons/apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" href="{{ URL::asset('favicons/favicon-32x32.png') }}" sizes="32x32">
+<link rel="icon" type="image/png" href="{{ URL::asset('favicons/favicon-194x194.png') }}" sizes="194x194">
+<link rel="icon" type="image/png" href="{{ URL::asset('favicons/android-chrome-192x192.png') }}" sizes="192x192">
+<link rel="icon" type="image/png" href="{{ URL::asset('favicons/favicon-16x16.png') }}" sizes="16x16">
+<link rel="manifest" href="{{ URL::asset('favicons/manifest.json') }}">
+<link rel="mask-icon" href="{{ URL::asset('favicons/safari-pinned-tab.svg') }}" color="#195128">
+<link rel="shortcut icon" href="{{ URL::asset('favicons/favicon.ic') }}o">
+<meta name="msapplication-TileColor" content="#00a300">
+<meta name="msapplication-TileImage" content="{{ URL::asset('favicons/mstile-144x144.png') }}">
+<meta name="msapplication-config" content="{{ URL::asset('favicons/browserconfig.xml') }}">
+<meta name="theme-color" content="#ffffff">
