df0a68b117c3beca866f646769bce1893ce94cf7
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-04-25 08:39:08 -0300

    Reorganização dos favicons

--- resources/views/partials/favicons.blade.php
+++ resources/views/partials/favicons.blade.php
@@ -1,12 +1,12 @@
-<link rel="apple-touch-icon" sizes="180x180" href="{{ URL::asset('images/favicons/apple-touch-icon.png') }}">
-<link rel="icon" type="image/png" href="{{ URL::asset('images/favicons/favicon-32x32.png') }}" sizes="32x32">
-<link rel="icon" type="image/png" href="{{ URL::asset('images/favicons/favicon-194x194.png') }}" sizes="194x194">
-<link rel="icon" type="image/png" href="{{ URL::asset('images/favicons/android-chrome-192x192.png') }}" sizes="192x192">
-<link rel="icon" type="image/png" href="{{ URL::asset('images/favicons/favicon-16x16.png') }}" sizes="16x16">
-<link rel="manifest" href="{{ URL::asset('images/favicons/manifest.json') }}">
-<link rel="mask-icon" href="{{ URL::asset('images/favicons/safari-pinned-tab.svg') }}" color="#195128">
-<link rel="shortcut icon" href="{{ URL::asset('images/favicons/favicon.ic') }}o">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ URL::asset('favicons/apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" href="{{ URL::asset('favicons/favicon-32x32.png') }}" sizes="32x32">
+<link rel="icon" type="image/png" href="{{ URL::asset('favicons/favicon-194x194.png') }}" sizes="194x194">
+<link rel="icon" type="image/png" href="{{ URL::asset('favicons/android-chrome-192x192.png') }}" sizes="192x192">
+<link rel="icon" type="image/png" href="{{ URL::asset('favicons/favicon-16x16.png') }}" sizes="16x16">
+<link rel="manifest" href="{{ URL::asset('favicons/manifest.json') }}">
+<link rel="mask-icon" href="{{ URL::asset('favicons/safari-pinned-tab.svg') }}" color="#195128">
+<link rel="shortcut icon" href="{{ URL::asset('favicons/favicon.ic') }}o">
 <meta name="msapplication-TileColor" content="#00a300">
-<meta name="msapplication-TileImage" content="{{ URL::asset('images/favicons/mstile-144x144.png') }}">
-<meta name="msapplication-config" content="{{ URL::asset('images/favicons/browserconfig.xml') }}">
+<meta name="msapplication-TileImage" content="{{ URL::asset('favicons/mstile-144x144.png') }}">
+<meta name="msapplication-config" content="{{ URL::asset('favicons/browserconfig.xml') }}">
 <meta name="theme-color" content="#ffffff">
