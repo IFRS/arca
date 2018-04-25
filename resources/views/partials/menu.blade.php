16e53b1cb636666ece38c430baad706c18305759
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-04-25 16:31:15 -0300

    Remoção de verificação já feita no layout

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,43 +1,41 @@
-@if (Auth::check())
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="{{ route('index') }}">
-            <img src="{{ URL::asset('images/logo-if.png') }}" alt="Logo dos Institutos Federais" width="40" height="40" class="img-fluid">
-            aRCA
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+<nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="{{ route('index') }}">
+        <img src="{{ URL::asset('images/logo-if.png') }}" alt="Logo dos Institutos Federais" width="40" height="40" class="img-fluid">
+        aRCA
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('cursos.index') }}">Cursos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('ofertas.index') }}">Ofertas</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Informa&ccedil;&otilde;es
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Campi</a>
-                        <a class="dropdown-item" href="#">Modalidades</a>
-                        <a class="dropdown-item" href="#">Níveis</a>
-                        <a class="dropdown-item" href="#">Turnos</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Sobre</a>
-                </li>
-            </ul>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                {{ csrf_field() }}
-                <span class="navbar-text">
-                    Olá, {{ Auth::user()->name }}
-                </span>
-                <button type="submit" class="btn btn-light" title="Sair"><i class="fas fa-sign-out-alt"></i></button>
-            </form>
-        </div>
-    </nav>
-@endif
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('cursos.index') }}">Cursos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('ofertas.index') }}">Ofertas</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Informa&ccedil;&otilde;es
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Campi</a>
+                    <a class="dropdown-item" href="#">Modalidades</a>
+                    <a class="dropdown-item" href="#">Níveis</a>
+                    <a class="dropdown-item" href="#">Turnos</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Sobre</a>
+            </li>
+        </ul>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+            {{ csrf_field() }}
+            <span class="navbar-text">
+                Olá, {{ Auth::user()->name }}
+            </span>
+            <button type="submit" class="btn btn-light" title="Sair"><i class="fas fa-sign-out-alt"></i></button>
+        </form>
+    </div>
+</nav>
