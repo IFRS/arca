6cbc3c1ce03382f9c800ac3e30286e18147d902e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-04-25 15:31:16 -0300

    Criação de seed para o usuário administrador padrão

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UsersTableSeeder::class);
         $this->call(TurnosTableSeeder::class);
         $this->call(ModalidadesTableSeeder::class);
         $this->call(NiveisTableSeeder::class);
         $this->call(CampiTableSeeder::class);
-        // $this->call(UsersTableSeeder::class);
     }
 }
