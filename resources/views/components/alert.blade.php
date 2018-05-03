701c4ec8d6f40b7a16fe7acb17391569ad8b149e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-05-03 15:37:53 -0300

    Adição de opção para exibir a ação de fechar no componente para alerts

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,6 +1,8 @@
 <div class="alert alert-{{ $status }}">
     {{ $slot }}
-    <button type="button" class="close" data-dismiss="alert" aria-label="Fechar">
-        <span aria-hidden="true">&times;</span>
-    </button>
+    @if (!isset($dismissable) || $dismissable)
+        <button type="button" class="close" data-dismiss="alert" aria-label="Fechar">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    @endif
 </div>
