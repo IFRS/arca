8c7151f6c3e4ad62672ddd80415f217b9e5c6642
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-05-03 15:39:00 -0300

    Ajuste nos nomes dos campos para arquivos e exibição dos erros relacionados

--- resources/views/forms/_files.blade.php
+++ resources/views/forms/_files.blade.php
@@ -3,12 +3,27 @@
     <div class="form-group">
         <label for="inputFileTitle" class="sr-only">T&iacute;tulo do arquivo</label>
         <div class="input-group">
-            <input id="inputFileTitle" type="text" name="file[title][]" class="form-control" placeholder="Título">
+            <input id="inputFileTitle" type="text" name="file_title[]" class="form-control" placeholder="Título">
             <div class="input-group-append">
                 <button class="btn btn-danger btn-removeFile" title="Remover novo arquivo"><i class="fas fa-minus"></i><span class="sr-only">Remover novo arquivo</span></button>
             </div>
         </div>
-        <input id="inputFile" type="file" name="file[file][]" class="form-control-file">
+        <input id="inputFile" type="file" name="file[]" class="form-control-file">
         <label for="inputFile" class="btn btn-info btn-block">Selecione um arquivo</label>
+
+        @foreach ($errors->get('file_title.*') as $messages)
+            @foreach ($messages as $message)
+                <div class="invalid-feedback d-block">
+                    {{ $message }}
+                </div>
+            @endforeach
+        @endforeach
+        @foreach ($errors->get('file.*') as $messages)
+            @foreach ($messages as $message)
+                <div class="invalid-feedback d-block">
+                    {{ $message }}
+                </div>
+            @endforeach
+        @endforeach
     </div>
 </fieldset>
