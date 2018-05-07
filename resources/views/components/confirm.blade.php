e080b786a23ecd06b27daac1104c135f9fe74fdc
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-05-07 11:06:02 -0300

    Criação de modal para confirmação, para uso futuro

---
+++ resources/views/components/confirm.blade.php
@@ -0,0 +1,13 @@
+<div class="modal fade" id="{{ $id or bcrypt(microtime()) }}" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title">{{ $title or 'Modal' }}</h3>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+                {{ $slot }}
+            </div>
+        </div>
+    </div>
+</div>
