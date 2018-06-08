ffa4fdfb63dc3efe52ef2fbc45434cd1815ffe2c
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-06-08 15:45:40 -0300

    Adição de opções para escolher role do usuário

--- resources/views/forms/user.blade.php
+++ resources/views/forms/user.blade.php
@@ -3,9 +3,26 @@
     {{ csrf_field() }}
 
     <div class="form-row">
+        <fieldset class="col-12">
+            <div class="form-check form-check-inline">
+                <input name="role" value="administrator" type="radio" class="form-check-input" id="checkTurnoAdministrator">
+                <label for="checkTurnoAdministrator" class="form-check-label">Administrador</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input name="role" value="manager" type="radio" class="form-check-input" id="checkTurnoManager">
+                <label for="checkTurnoManager" class="form-check-label">Gerente</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input name="role" value="editor" type="radio" class="form-check-input" id="checkTurnoEditor" checked>
+                <label for="checkTurnoEditor" class="form-check-label">Cadastrador</label>
+            </div>
+        </fieldset>
+    </div>
+
+    <div class="form-row">
         <div class="col-12 form-group">
-            <label for="inputUsername">Uid (Nome de Usu&aacute;rio)</label>
-            <input type="text" name="username" value="{{ old('username', $user->username) }}" id="inputUsername" class="form-control{{ count($errors->get('username')) > 0 ? ' is-invalid' : '' }}">
+            <label for="inputUsername" class="sr-only">Uid (Nome de Usu&aacute;rio)</label>
+            <input type="text" name="username" placeholder="Uid (nome de usuário)" value="{{ old('username', $user->username) }}" id="inputUsername" class="form-control{{ count($errors->get('username')) > 0 ? ' is-invalid' : '' }}">
             @foreach ($errors->get('username') as $message)
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -19,7 +36,7 @@
             <a href="{{ url()->previous() }}" class="btn btn-secondary">Cancelar</a>
         </div>
         <div class="col-12 col-md-6 text-right">
-            <button type="submit" class="btn btn-primary">Adicionar</button>
+            <button type="submit" class="btn btn-success">Adicionar</button>
         </div>
     </div>
 </form>
