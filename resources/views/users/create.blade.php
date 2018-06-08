a20569baa7a5127092af98b0e03d49ee2ce9ed66
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-06-08 15:44:33 -0300

    Ajuste para clareza

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="row">
         <div class="col-12">
-            <h2>Novo Usu&aacute;rio</h2>
+            <h2>Novo Usu&aacute;rio LDAP</h2>
             <hr>
         </div>
     </div>
